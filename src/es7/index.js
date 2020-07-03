519295a5601805fae5da1b8e227f8afbdc2723f6
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-07-02 22:40:10 -0500

    finish es7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 7, 8]
+let num = 9
+if (numbers.includes(num)) {
+  console.log(`si se encuentra el valor ${num}`)
+} else {
+  console.log(`el numero ${num} no se encuentra`)
+}
+let base = 4
+let exponent = 3
+let result = base ** exponent
+
+console.log(result)
